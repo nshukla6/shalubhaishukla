0c92a7da49e360b5b77aa3e9efc18cca0b8fcae2
Nitin Shukla <nitins@vmware.com>
2016-05-04 00:28:22 +0530

    new

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app=express();
 app.get('/',function(request,response){
   response.sendFile(__dirname+'/para3col.html');
 });
-
+app.use(express.static(__dirname + '/public'));
 var port = process.env.PORT || 8080;
 
 var server=app.listen(port,function(req,res){
